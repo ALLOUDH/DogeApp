f74753dc1cf7a532e26e63bf18e738a8e7c6aa84
Hector Chavez <48160099+ALLOUDH@users.noreply.github.com>
2023-06-21 13:27:17 -0500

    Carga de recursos

---
+++ app/src/main/java/des/app/dogeapp/Razas.java
@@ -0,0 +1,14 @@
+package des.app.dogeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Razas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_razas);
+    }
+}
